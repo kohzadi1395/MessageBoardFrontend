c51384dfb6f9dee42e5d53f48553fb5dacaadf54
kohzadi <kohzadi_hossein@yahoo.com>
2019-03-13 12:03:10 +0330

    initial project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {MessageComponentComponent} from './message-component/message-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MessageComponentComponent
   ],
   imports: [
     BrowserModule,
